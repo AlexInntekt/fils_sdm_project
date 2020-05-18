016d17d4689ca811899ffbd0c6e081d6a388e0df
alexinntekt <alex.inntekt@gmail.com>
2020-05-19 01:00:24 +0300

    sp

--- src/hotel/urls.py
+++ src/hotel/urls.py
@@ -2,12 +2,13 @@ from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings 
 
-from .views import RoomsListView, RoomDetailView, AdminView
+from .views import AdminView, RoomsListView, RoomDetailView, RoomAddView
 
 urlpatterns = [
     path('', AdminView.as_view(), name='admin'),
     path('rooms', RoomsListView.as_view(), name='rooms'),
     path('room/id=<int:id>', RoomDetailView.as_view(), name='room'),
+    path('rooms/add', RoomAddView.as_view(), name='addroom')
 
 ]
 
--- src/hotel/views.py
+++ src/hotel/views.py
@@ -21,6 +21,39 @@ class RoomsListView(TemplateView):
 
         return render(request, self.template_name, context)
 
+class RoomAddView(TemplateView):
+    template_name = 'add_room.html'
+
+    def get(self, request, *args, **kwargs):
+
+        context = {}
+
+        return render(request, self.template_name, context)
+
+    def post(self, request, *args, **kwargs):
+        context = {}
+        post = request.POST
+        print(post)
+
+        try:
+            bed_type = post['bed_type']
+            facilities = post['facilities']
+            price = post['price']
+
+            room = Room()
+
+            room.bed_type = bed_type
+            room.facilities = facilities
+            room.price = price
+            room.save()
+
+            # context['room'] = room
+            context['success'] = "The change was applied succesfully!"
+        except Exception as e:
+            context['error'] = "One problem occured: "+str(e)
+        return render(request, self.template_name, context)
+
+
 
 class RoomDetailView(TemplateView):
     template_name = 'room.html'
@@ -46,7 +79,7 @@ class RoomDetailView(TemplateView):
             if action == 'delete':
                 room = Room.objects.get(id=room_id)
                 room.delete()
-                return redirect('admin')
+                return redirect('rooms')
 
             else:
                 bed_type = post['bed_type']
