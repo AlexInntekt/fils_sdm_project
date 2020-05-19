62d6bc9bab2244f74bfb43b2d105d0eb40e0aa88
alexinntekt <alex.inntekt@gmail.com>
2020-05-19 09:47:20 +0300

    sp

--- src/hotel/views.py
+++ src/hotel/views.py
@@ -1,8 +1,10 @@
 from datetime import datetime
+from collections import namedtuple
 
 from django.shortcuts import render
 from django.views.generic import TemplateView
 from django.shortcuts import redirect
+from django.db.models import Q
 
 from .models import Room, Booking, Checkin
 
@@ -21,6 +23,8 @@ class CheckinEditView(TemplateView):
 
         context['checkin'] = checkin
         return render(request, self.template_name, context)
+
+
 
     def post(self, request, *args, **kwargs):
         context = {}
@@ -43,6 +47,7 @@ class CheckinEditView(TemplateView):
                 end_date = post['end_date']
 
                 checkin = Checkin.objects.get(id=checkin_id)
+                context['checkin'] = checkin
 
                 checkin.room = room
                 checkin.customer = customer
@@ -50,16 +55,39 @@ class CheckinEditView(TemplateView):
                 checkin.end_datetime = datetime.strptime(str(end_date),'%Y-%m-%d')
                 checkin.last_modified = datetime.now()
 
-                Qd = Q((room__id=checkin.room.id) && ())
-                current_checkins = Checkin.objects.filter(Qd)
-                checkin.save()
+                Qd = Q() # & ~Q(id=checkin.id)
 
-                context['checkin'] = checkin
-                context['success'] = "The change was applied succesfully!"
+                Qd |= Q(Q(Q(start_datetime__range=[starting_date, end_date]) | Q(end_datetime__range=[starting_date, end_date])) & Q(room__id=checkin.room.id) & ~Q(id=checkin.id))
+                Qd |= Q(Q(Q(start_datetime__lte=starting_date) & Q(end_datetime__gte=end_date)) & Q(room__id=checkin.room.id) & ~Q(id=checkin.id))
+                Qd |= Q(Q(Q(start_datetime__gte=starting_date) & Q(end_datetime__lte=end_date)) & Q(room__id=checkin.room.id) & ~Q(id=checkin.id))
+
+                current_checkins = Checkin.objects.filter(Qd)
+                objs = current_checkins.count()
+                print("\n\n")
+                print(objs)
+                if(objs==0):
+                    checkin.save()
+                    context['checkin'] = checkin
+                    context['success'] = "The change was applied succesfully!"
+                else:
+                    context['error'] = "Some old checkins overlap!"
         except Exception as e:
             context['error'] = "One problem occured: "+str(e)
         return render(request, self.template_name, context)
 
+    def do_dates_overlap(cdate1,cdate2,ddate1,ddate2):
+        Range = namedtuple('Range', ['start', 'end'])
+        r1 = Range(start=datetime(cdate1), end=datetime(cdate2))
+        r2 = Range(start=datetime(ddate1), end=datetime(ddate2))
+        latest_start = max(r1.start, r2.start)
+        earliest_end = min(r1.end, r2.end)
+        delta = (earliest_end - latest_start).days + 1
+        overlap = max(0, delta)
+
+        if(overlap>0):
+            return True
+        else:
+            return False
 
 class CheckinsListView(TemplateView):
     template_name = 'checkins_list.html'
